d5d8522c03897a61f1efc9657e156b504beca57a
Axel B <barbieraxel@gmail.com>
2019-12-12 14:48:58 +0100

    réglages

--- Coode/Assets/Scripts/Level/CamMove.cs
+++ Coode/Assets/Scripts/Level/CamMove.cs
@@ -7,11 +7,29 @@ public class CamMove : MonoBehaviour
     public Transform player;
     public int height;
 
+    private Camera cam;
+
+
+    private void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+
     void Update()
     {
         transform.position = Vector3.Lerp(
             transform.position, 
             new Vector3(player.position.x, player.position.y + height, player.position.z - height/2),
             5 * Time.deltaTime);
+
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f && cam.fieldOfView > 50) // forward
+        {
+            cam.fieldOfView -= 4;
+        }
+        else if (Input.GetAxis("Mouse ScrollWheel") < 0f && cam.fieldOfView < 70) // backwards
+        {
+            cam.fieldOfView += 4;
+        }
     }
 }
